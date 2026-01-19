2a37e247fe644a441e7781381f94ea8103380b53
toliveira1802-sketch <toliveira1802@gmail.com>
2026-01-19 12:26:35 -0300

    fix: listen on PORT env variable

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+ const port = process.env.PORT || 3000;
+await app.listen(port, '0.0.0.0');
+console.log(`🚀 Application is running on: http://0.0.0.0:${port}` );
+
 }
 bootstrap();
